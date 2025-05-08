1ef8c28c7f2d3231cc0618e2d737e5a37a9afdb0
Gapmanhome <gapmanhome@gmail.com>
2025-05-08 09:10:11 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = { experimental: { appDir: true } };
+const nextConfig = {
+  experimental: { appDir: true },
+  compiler: {
+    // Enable the SWC compiler to use the "module" type
+    swcMinify: true,
+  },
+};
 export default nextConfig;
